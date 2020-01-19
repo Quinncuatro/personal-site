4af89be096c8a2993376134e912c8ee3cce2652d
timhwang21 <timhwang21@gmail.com>
2020-01-19 16:27:39 -0500

    Fix animation

--- src/components/currentLogin.js
+++ src/components/currentLogin.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { graphql, StaticQuery } from "gatsby"
 import currentLoginStyles from "./currentLogin.module.css"
 import BusinessCard from "./businessCard"
@@ -8,27 +8,116 @@ import Education from "./education"
 import Presentation from "./presentation"
 import Footer from "./footer"
 
-let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+]
 
 function zerosAreHard(n) {
   if (n < 10) {
-    n = "0" + n;
+    n = "0" + n
   }
-  return n;
+  return n
 }
 
-let now = new Date();
-let day = days[ now.getDay() ];
-let month = months[ now.getMonth() ];
-let date = now.getDate();
-let hours = zerosAreHard(now.getHours());
-let minutes = zerosAreHard(now.getMinutes());
-let seconds = zerosAreHard(now.getSeconds());
-let year = now.getFullYear();
+let now = new Date()
+let day = days[now.getDay()]
+let month = months[now.getMonth()]
+let date = now.getDate()
+let hours = zerosAreHard(now.getHours())
+let minutes = zerosAreHard(now.getMinutes())
+let seconds = zerosAreHard(now.getSeconds())
+let year = now.getFullYear()
 
-let currentLoginPrompt = "Current login: " + day + " " + month + " " + date + " " + hours +":" + minutes + ":" + seconds + " " + year;
-  
+let currentLoginPrompt =
+  "Current login: " +
+  day +
+  " " +
+  month +
+  " " +
+  date +
+  " " +
+  hours +
+  ":" +
+  minutes +
+  ":" +
+  seconds +
+  " " +
+  year
+
+const CurrentLogin = data => {
+  const [hidden, setHidden] = useState(true)
+  useEffect(() => {
+    setTimeout(() => setHidden(false), 1000)
+  }, [])
+
+  return (
+    <div>
+      <span>
+        {currentLoginPrompt}
+        <br />
+        {console.log(data)}
+        Welcome to HenryNeeds.Coffee!
+      </span>
+      <br />
+      <br />
+      [hquinn@HenryNeeds ~]${" "}
+      <span className={currentLoginStyles.typed}>
+        &nbsp;whoami<span>&nbsp;</span>
+      </span>
+      <div className={hidden ? currentLoginStyles.hiddenPart : ""}>
+        <BusinessCard />
+        <h3>Work Experience:</h3>
+        {data.allContentYaml.edges[0].node.jobs.map(job => (
+          <Job
+            employer={job.employer}
+            position={job.position}
+            time={job.time}
+            details={job.details}
+          />
+        ))}
+        <hr />
+        <h3>Projects:</h3>
+        {data.allContentYaml.edges[0].node.projects.map(project => (
+          <Project
+            name={project.name}
+            year={project.year}
+            description={project.description}
+            technologies={project.technologies}
+            github={project.github}
+            dockerhub={project.dockerhub}
+          />
+        ))}
+        <hr />
+        <h3>Education:</h3>
+        <Education />
+        <hr />
+        <h3>National Presentations:</h3>
+        {data.allContentYaml.edges[0].node.presentations.map(presentation => (
+          <Presentation
+            talk={presentation.talk}
+            conference={presentation.conference}
+            location={presentation.location}
+            date={presentation.date}
+          />
+        ))}
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
 export default () => (
   <StaticQuery
     query={graphql`
@@ -61,51 +150,7 @@ export default () => (
         }
       }
     `}
-    render={data => (
-      <div>
-        <span >{ currentLoginPrompt }<br />{console.log(data)}
-        Welcome to HenryNeeds.Coffee!</span>
-        <br /><br />
-        [hquinn@HenryNeeds ~]$ <span className={currentLoginStyles.typed}>&nbsp;whoami<span>&nbsp;</span></span>
-        <div className={currentLoginStyles.hiddenPart}>
-          <BusinessCard />
-          <h3>Work Experience:</h3>
-          {data.allContentYaml.edges[0].node.jobs.map((job) => (
-            <Job
-            employer={job.employer}
-            position={job.position}
-            time={job.time}
-            details={job.details}
-          />
-          ))}
-          <hr />
-          <h3>Projects:</h3>
-          {data.allContentYaml.edges[0].node.projects.map((project) => (
-            <Project
-              name={project.name}
-              year={project.year}
-              description={project.description}
-              technologies={project.technologies}
-              github={project.github}
-              dockerhub={project.dockerhub}
-            />
-          ))}
-          <hr />
-          <h3>Education:</h3>
-          <Education />
-          <hr />
-          <h3>National Presentations:</h3>
-          {data.allContentYaml.edges[0].node.presentations.map((presentation) => (
-            <Presentation
-              talk={presentation.talk}
-              conference={presentation.conference}
-              location={presentation.location}
-              date={presentation.date}
-            />
-          ))}
-          <Footer />
-        </div>
-      </div>
-    )}
+    render={CurrentLogin}
   />
 )
+
